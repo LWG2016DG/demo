07dc5429c9fdc66ee4de17332c4f028eafb04efe
LWG2016DG <1360279694@qq.com>
2016-11-09 20:14:09 +0800

    apple

---
+++ apple.js
@@ -0,0 +1,15 @@
+/**
+ * Created by pc on 2016/10/22.
+ */
+$(document).ready( function() {
+
+    $('#fullpage').fullpage({
+        verticalCentered: false,
+        anchors: ['page1','page2','page3','page4'],
+        navigation: true,
+        navigationTooltips: ['它，终于来了','真正与你贴近的个人设备','非同一般的精准计时','在三个特点鲜明的系列中找到你的风格']
+
+
+    });
+
+});
